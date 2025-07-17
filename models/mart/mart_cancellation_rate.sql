5d77cca20c85157fc4bfafa9e72e790e7d0ff978
Heiko Graceffa <heiko.graceffa@it-tem.de>
2025-07-17 15:40:15 +0200

    added mart_cancellation_rate sql

---
+++ models/mart/mart_cancellation_rate.sql
@@ -0,0 +1,16 @@
+-- airport information, cancelled, date, diverted, wetterbedindung (windspeed) correlation
+WITH cancellationrate AS (
+    SELECT 	
+        origin,
+        dest,
+        flight_date,
+        SUM(cancelled) AS dep_cancelled,
+        SUM(diverted) AS dep_diverted
+    FROM {{ ref('prep_flights') }}
+    WHERE cancelled IS NOT NULL AND diverted IS NOT NULL 
+	AND flight_date BETWEEN '2012-10-25' AND '2021-11-05'
+    GROUP BY flight_date, origin, dest
+    ORDER BY flight_date 
+) 
+SELECT * 
+FROM cancellationrate;
